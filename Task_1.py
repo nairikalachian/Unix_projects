795e13424aa7ebf8acb8a5c5b6bd31ae3cc871a6
Anastasiia <st0970960@student.spbu.ru>
2025-10-08 20:42:22 +0000

    Nastya changed the file

--- Task_1.py
+++ Task_1.py
@@ -1,5 +1,3 @@
-# Task 1
-
 text = "Before departure, all students leaving for exchange studies should fill in a specialized application form. An application should be submitted in 1 copy and not later than 2 weeks prior to departure for the exchange semester, provided that the students do not have any academic failure. The paper should be submitted to the International Academic Mobility Office (Volkhovsky per., 3, office 207, Tel.: +7 (812) 323 8447). A copy of the invitation letter from a host school - partner of GSOM SPbU has to be attached to the application."
 
 st_ind = text.find("(") + 1 # Nastya removed variables and entered needed text rightaway 
